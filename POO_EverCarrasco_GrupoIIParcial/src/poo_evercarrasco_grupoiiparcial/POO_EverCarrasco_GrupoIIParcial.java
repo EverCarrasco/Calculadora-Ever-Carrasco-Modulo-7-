c7088f10a6da27cac13b679c319533bac8729a7f
ever.carrasco1405@gmail.com <>
2019-11-16 19:14:42 -0600

    Merge origin/master

--- POO_EverCarrasco_GrupoIIParcial/src/poo_evercarrasco_grupoiiparcial/POO_EverCarrasco_GrupoIIParcial.java
+++ POO_EverCarrasco_GrupoIIParcial/src/poo_evercarrasco_grupoiiparcial/POO_EverCarrasco_GrupoIIParcial.java
@@ -5,9 +5,11 @@
  */
 package poo_evercarrasco_grupoiiparcial;
 
+import java.util.Scanner;
+
 /**
  *
- * @author Ever
+ * @author Ever¿p
  */
 public class POO_EverCarrasco_GrupoIIParcial {
 
@@ -16,6 +18,25 @@ public class POO_EverCarrasco_GrupoIIParcial {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+         // TODO code application logic here
+        Scanner s = new Scanner(System.in);
+  
+        System.out.println("ESTUDIANTE");
+    
+    Doctor e= new Doctor ("default",0,0,"default");
+    e.ingreso_datospersonales();
+    e.ingreso_datocadena();
+    e.ingreso_datonumerico();
+    e.muestrodatos();
+    
+        System.out.println("");
+        System.out.println("");
+        System.out.println("DEPORTISTAS");
+    Deportista d= new Deportista("default",0,0,"default");
+    d.ingreso_datospersonales();
+    d.ingreso_datocadena();
+    d.ingreso_datonumerico();
+    d.muestrodatos();
     }
     
 }
